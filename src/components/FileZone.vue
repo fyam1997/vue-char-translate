ccb2cc27ec2988493bfb0b942852fa7e725adb81
fyam <fung19971115@gmail.com>
2025-05-21 12:49:04 +0800

    Add file zone

---
+++ src/components/FileZone.vue
@@ -0,0 +1,81 @@
+<script setup lang="ts">
+import {useTemplateRef} from "vue";
+import {useDropZone, useFileDialog} from "@vueuse/core";
+
+const props = defineProps<{
+  accept: string
+}>()
+const emits = defineEmits<{
+  onFileSelected: [File],
+}>()
+
+const {open, onChange, reset} = useFileDialog({
+  accept: props.accept,
+  directory: false,
+  multiple: false,
+})
+
+const dropZoneRef = useTemplateRef("dropZoneRef")
+
+const zone = useDropZone(dropZoneRef, {
+  onDrop: onDrop,
+  dataTypes: props.accept.split(","),
+  multiple: false,
+})
+const isOverDropZone = zone.isOverDropZone
+
+
+async function onDrop(files: File[] | null) {
+  if (files) {
+    emits("onFileSelected", files[0])
+  }
+}
+
+onChange(async (files) => {
+  if (files) {
+    emits("onFileSelected", files[0])
+    reset()
+  }
+})
+
+</script>
+
+<template>
+  <div ref="dropZoneRef" class="position-relative">
+    <slot name="default" :openFile="open"/>
+
+    <Transition>
+      <h1 v-show="isOverDropZone" class="drop-target-overlay">
+        Drop file here
+      </h1>
+    </Transition>
+  </div>
+</template>
+
+<style scoped>
+.drop-target-overlay {
+  position: absolute;
+  inset: 0;
+  /*noinspection CssUnresolvedCustomProperty*/
+  background: rgba(var(--v-theme-on-surface), 0.32);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 4px;
+  /*noinspection CssUnresolvedCustomProperty*/
+  border: 2px dashed rgba(var(--v-border-color), var(--v-border-opacity));
+}
+
+/*noinspection CssUnusedSymbol*/
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.2s ease;
+}
+
+/*noinspection CssUnusedSymbol*/
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+
+</style>
