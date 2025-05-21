b94ce75d70dbea29f620aef0fbafb00a7558fb67
fyam <fung19971115@gmail.com>
2025-05-21 14:56:44 +0800

    Add result panel

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import {ViewModel} from "@/viewmodel/ViewModel.ts";
 import {provide} from "vue";
-import SimpleUI from "@/components/SimpleUI.vue";
 import ConfigPanel from "@/components/ConfigPanel.vue";
 import RawPanel from "@/components/RawPanel.vue";
+import ResultPanel from "@/components/ResultPanel.vue";
 
 const viewModel = new ViewModel()
 provide("viewModel", viewModel)
@@ -15,12 +15,12 @@ provide("viewModel", viewModel)
     <v-snackbar-queue v-model="viewModel.snackbarMessages.value" location="top"></v-snackbar-queue>
     <div class="w-100 h-100 d-flex flex-row ga-2 justify-center">
       <ConfigPanel class="config-panel-large w-100 h-100 pa-4"/>
-      <div class="raw-panel-large w-100 h-100 d-flex flex-column">
+      <div class="content-panel-large flex-grow-1 pa-4 d-flex flex-column">
         <RawPanel class="raw-panel-large w-100 flex-grow-1 pa-4"/>
         <v-divider/>
         <v-btn class="ma-4" variant="outlined" @click="viewModel.translate()" text="Translate"/>
       </div>
-      <SimpleUI class="flex-grow-1"/>
+      <ResultPanel class="content-panel-large flex-grow-1 pa-4"/>
     </div>
   </v-app>
 </template>
@@ -30,7 +30,7 @@ provide("viewModel", viewModel)
   max-width: 300px;
 }
 
-.raw-panel-large {
+.content-panel-large {
   max-width: 600px;
 }
 </style>
---
+++ src/components/ResultPanel.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import {inject} from "vue";
+import {ViewModel} from "@/viewmodel/ViewModel.ts";
+
+const viewModel = inject<ViewModel>("viewModel")
+const translatedJson = viewModel.translatedJson
+
+</script>
+
+<template>
+  <div class="h-100 d-flex flex-column ga-2">
+    <h4>Translated</h4>
+    <v-textarea v-model="translatedJson" no-resize hide-details class="flex-grow-1"/>
+    <div class="d-flex flex-row ga-2 mb-4">
+      <v-btn @click="viewModel.downloadJson()" text="downloadJson" class="text-none flex-grow-1" variant="outlined"/>
+      <v-btn @click="viewModel.downloadImage()" text="downloadImage" class="text-none flex-grow-1" variant="outlined"/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
