089b1214b0ed7397255faa09d206f4e78d319762
fyam <fung19971115@gmail.com>
2025-05-24 23:41:01 +0800

    Temp Fix default prompt not found when deployed

--- src/viewmodel/TranslationStorage.ts
+++ src/viewmodel/TranslationStorage.ts
@@ -3,7 +3,19 @@ import {useIndexedDB} from "@/shared/db/DatabaseUtils.ts";
 import {ref} from "vue";
 
 export class TranslationStorage {
-    prompt = translationData("prompt", "", 200)
+    prompt = translationData("prompt", "You are an expert translator who translates English to Traditional Chinese.\n" +
+        "You pay attention to style, formality, idioms, slang etc. and try to convey it in the way a Traditional Chinese speaker would understand.\n" +
+        "BE MORE NATURAL.\n" +
+        "Specifically, you will be translating text from a role play character card.\n" +
+        "To aid you and provide context, You will be given a json of the character card. Return the json with the texts translated.\n" +
+        "DO NOT translate the json keys.\n" +
+        "DO NOT respond in Markdown format like ```json ```.\n" +
+        "DO NOT give explanations.\n" +
+        "If it's already in Traditional Chinese looks like gibberish, OUTPUT IT AS IT IS instead.\n" +
+        "Translate all character name.\n" +
+        "replace all \"\" with 「」 inside json values.\n" +
+        "DO NOT USE ＊.\n" +
+        "DO NOT translate tags.", 200)
     raw = translationData("raw", {}, 200)
     translated = translationData("translated", {}, 200)
     image = translationData<Uint8Array>("image", null)
