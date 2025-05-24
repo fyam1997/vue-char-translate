d978230b63e178b28a3f8969a1123bc1c627df23
fyam <fung19971115@gmail.com>
2025-05-25 00:47:35 +0800

    Update file zone layout

--- src/components/RawJsonPanel.vue
+++ src/components/RawJsonPanel.vue
@@ -1,35 +1,30 @@
 <script setup lang="ts">
 
-import FileZone from "@/components/FileZone.vue";
 import {inject} from "vue";
 import {ViewModel} from "@/viewmodel/ViewModel.ts";
 import EditObjectDialog from "@/components/EditObjectDialog.vue";
 import FlattenJsonList from "@/components/FlattenJsonList.vue";
+import {UseFileDialogOptions} from "@vueuse/core";
 
 const viewModel = inject<ViewModel>("viewModel")
 
 const rawJson = viewModel.rawJson
 
+const openFile = inject<(localOptions?: Partial<UseFileDialogOptions>) => void>("openFileCallback")
 </script>
 
 <template>
-  <FileZone
-      accept="application/json"
-      class="pa-4"
-      @onFileSelected="(file)=>viewModel.loadFile(file)"
-  >
-    <template v-slot:default="{openFile}">
-      <div class="pb-4 d-flex flex-row align-center">
-        <h4>Json</h4>
-        <v-spacer/>
-        <EditObjectDialog :obj="rawJson.getSrcValue()" @save="obj=>viewModel.setRawJson(obj)"/>
-        <v-icon-btn v-if="!rawJson.isEmpty()" icon="md:delete" variant="plain" @click="viewModel.clearJson()"
-                    title="Delete"/>
-        <v-icon-btn icon="md:upload" variant="plain" @click="openFile" title="Upload"/>
-      </div>
-      <FlattenJsonList :flatten-json="viewModel.rawJson"/>
-    </template>
-  </FileZone>
+  <div>
+    <div class="pb-4 d-flex flex-row align-center">
+      <h4>Json</h4>
+      <v-spacer/>
+      <EditObjectDialog :obj="rawJson.getSrcValue()" @save="obj=>viewModel.setRawJson(obj)"/>
+      <v-icon-btn v-if="!rawJson.isEmpty()" icon="md:delete" variant="plain" @click="viewModel.clearJson()"
+                  title="Delete"/>
+      <v-icon-btn icon="md:upload" variant="plain" @click="openFile" title="Upload"/>
+    </div>
+    <FlattenJsonList :flatten-json="viewModel.rawJson"/>
+  </div>
 </template>
 
 <style scoped>
