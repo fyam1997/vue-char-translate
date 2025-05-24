65229eeccf87fbd32cc622ce386307c0731ca850
fyam <fung19971115@gmail.com>
2025-05-25 01:06:33 +0800

    Update empty image ui

--- src/components/ImagePanel.vue
+++ src/components/ImagePanel.vue
@@ -12,21 +12,33 @@ const openFile = inject<(localOptions?: Partial<UseFileDialogOptions>) => void>(
 </script>
 
 <template>
-  <div>
+  <div v-if="imageSrc">
     <div class="pb-4 d-flex flex-row align-center">
       <h4>Character Image</h4>
       <v-spacer/>
-      <v-icon-btn v-if="imageSrc" icon="md:delete" variant="plain" @click="viewModel.clearImage()" title="Delete"/>
+      <v-icon-btn icon="md:delete" variant="plain" @click="viewModel.clearImage()" title="Delete"/>
       <v-icon-btn icon="md:upload" variant="plain" @click="openFile" title="Upload"/>
     </div>
     <v-img
-        v-if="imageSrc"
         :src="imageSrc"
         alt="Character image"
         title="Character image"
         class="ml-8 mr-8"
     />
   </div>
+
+  <!--just use v-file-upload UI, won't upload file through it-->
+  <v-file-upload
+      v-else
+      title="Drop Image Here"
+  >
+    <template v-slot:icon>
+      <v-icon icon="md:upload" size="50"/>
+    </template>
+    <template v-slot:browse>
+      <v-btn variant="text" @click="openFile" text="Browse Files" class="text-none" prepend-icon="md:folder_open"/>
+    </template>
+  </v-file-upload>
 </template>
 
 <style scoped>
